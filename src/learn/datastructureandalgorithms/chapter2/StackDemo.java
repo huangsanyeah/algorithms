112df28c9f9485011f496199f2df9297bc4c9c80
huangweiyue <huangweiyue@quyiyuan.com>
2021-01-09 14:26:40 +0800

    Stack回文数字

--- src/learn/datastructureandalgorithms/chapter2/StackDemo.java
+++ src/learn/datastructureandalgorithms/chapter2/StackDemo.java
@@ -2,15 +2,90 @@ package learn.datastructureandalgorithms.chapter2;
 
 import java.util.Stack;
 
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * 有效字符串需满足：左括号必须与相同类型的右括号匹配，左括号必须以正确的顺序匹配。
+ * 例如，{ [ ( ) ( ) ] } 是合法的，而 { ( [ ) ] } 是非法的。
+ */
 public class StackDemo {
     public static void main(String[] args) {
-        Stack stack=new Stack();
-        stack.add("1");
-        stack.add("2");
-        stack.add("3");
-        stack.add("4");
 
-        stack.peek();
-        System.out.println(stack);
+        String s = "{[()()]}";
+
+        System.out.println(isLegal(s));
+
     }
+
+    private static int isLeft(char c) {
+
+        if (c == '{' || c == '(' || c == '[') {
+
+            return 1;
+
+        } else {
+
+            return 2;
+
+        }
+
+    }
+
+    private static int isPair(char p, char curr) {
+
+        if ((p == '{' && curr == '}') || (p == '[' && curr == ']') || (p == '(' && curr == ')')) {
+
+            return 1;
+
+        } else {
+
+            return 0;
+
+        }
+
+    }
+
+    private static String isLegal(String s) {
+
+        Stack stack = new Stack();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char curr = s.charAt(i);
+
+            if (isLeft(curr) == 1) {
+
+                stack.push(curr);
+
+            } else {
+
+                if (stack.empty()) {
+
+                    return "非法";
+
+                }
+
+                char p = (char) stack.pop();
+
+                if (isPair(p, curr) == 0) {
+
+                    return "非法";
+
+                }
+
+            }
+
+        }
+
+        if (stack.empty()) {
+
+            return "合法";
+
+        } else {
+
+            return "非法";
+
+        }
+
+    }
+
 }
