5f863ca7bc47a77f8c02dda9535e513428b1e62e
hwy15972 <893663791@qq.com>
2021-02-18 14:22:21 +0800

    希尔排序注释

--- src/datastructure/sort/ShellSort.java
+++ src/datastructure/sort/ShellSort.java
@@ -1,32 +1,38 @@
 package datastructure.sort;
 
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Arrays;
 
+/**
+ * 希尔排序O(n logn)
+ */
 public class ShellSort {
 
     public static void main(String[] args) {
-        //int[] arr = { 8, 9, 1, 7, 2, 3, 5, 4, 6, 0 };
-
-        // 创建要给80000个的随机的数组
-        int[] arr = new int[8000000];
-        for (int i = 0; i < 8000000; i++) {
-            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
-        }
-
-        System.out.println("排序前");
-        Date data1 = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1Str = simpleDateFormat.format(data1);
-        System.out.println("排序前的时间是=" + date1Str);
-
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        System.out.println("排序前" + Arrays.toString(arr));
         //shellSort(arr); //交换式
         shellSort2(arr);//移位方式
+        System.out.println("排序后" + Arrays.toString(arr));
 
-        Date data2 = new Date();
-        String date2Str = simpleDateFormat.format(data2);
-        System.out.println("排序前的时间是=" + date2Str);
+        // 创建要给80000个的随机的数组
+//        int[] arr = new int[8000000];
+//        for (int i = 0; i < 8000000; i++) {
+//            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+//        }
+//
+//        System.out.println("排序前");
+//        Date data1 = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date1Str = simpleDateFormat.format(data1);
+//        System.out.println("排序前的时间是=" + date1Str);
+//
+//        //shellSort(arr); //交换式
+//        shellSort2(arr);//移位方式
+//
+//        Date data2 = new Date();
+//        String date2Str = simpleDateFormat.format(data2);
+//        System.out.println("排序前的时间是=" + date2Str);
 
         //System.out.println(Arrays.toString(arr));
     }
