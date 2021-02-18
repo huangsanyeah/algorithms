c959717060cde195feb1ad14e3d33672c5eebc79
hwy15972 <893663791@qq.com>
2021-02-18 13:54:58 +0800

    插入排序注释

--- src/datastructure/sort/InsertSort.java
+++ src/datastructure/sort/InsertSort.java
@@ -1,31 +1,32 @@
 package datastructure.sort;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Arrays;
 
 public class InsertSort {
 
     public static void main(String[] args) {
         //int[] arr = {101, 34, 119, 1, -1, 89};
         // 创建要给80000个的随机的数组
-        int[] arr = new int[80000];
-        for (int i = 0; i < 80000; i++) {
-            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
-        }
-
-        System.out.println("插入排序前");
-        Date data1 = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1Str = simpleDateFormat.format(data1);
-        System.out.println("排序前的时间是=" + date1Str);
-
-        insertSort(arr); //调用插入排序算法
-
-        Date data2 = new Date();
-        String date2Str = simpleDateFormat.format(data2);
-        System.out.println("排序前的时间是=" + date2Str);
+//        int[] arr = new int[80000];
+//        for (int i = 0; i < 80000; i++) {
+//            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+//        }
+//
+//        System.out.println("插入排序前");
+//        Date data1 = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date1Str = simpleDateFormat.format(data1);
+//        System.out.println("排序前的时间是=" + date1Str);
+//
+//        insertSort(arr); //调用插入排序算法
+//
+//        Date data2 = new Date();
+//        String date2Str = simpleDateFormat.format(data2);
+//        System.out.println("排序前的时间是=" + date2Str);
 
         //System.out.println(Arrays.toString(arr));
+
+        testInsertSort();
 
 
     }
@@ -116,6 +117,61 @@ public class InsertSort {
 		System.out.println("第3轮插入");
 		System.out.println(Arrays.toString(arr)); */
 
+    }
+
+    /**
+     * 插入排序（从小到大）练习
+     * 理解：每次循环都是拿一个元素与之前插入好的元素进行大小比较，跳出循环找到待插入的值后，赋值
+     * debug代码 理解流程
+     */
+    private static void testInsertSort() {
+        int[] arr = {101, 34, 99, 1};
+        //定义待插入的数
+        int insertVal = arr[1];
+        int insertIndex = 1 - 1; //即arr[1]的前面这个数的下标
+
+        //给insertVal 找到插入的位置
+        //说明
+        //1. insertIndex >= 0 保证在给insertVal 找插入位置，不越界
+        //2. insertVal < arr[insertIndex] 待插入的数，还没有找到插入位置
+        //3. 就需要将 arr[insertIndex] 后移
+        while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+            arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+            insertIndex--;
+        }
+        //当退出while循环时，说明插入的位置找到, insertIndex + 1
+        //举例：理解不了，我们一会 debug
+        arr[insertIndex + 1] = insertVal;
+
+        System.out.println("第1轮插入");
+        System.out.println(Arrays.toString(arr));
+
+
+        //第2轮
+        insertVal = arr[2];
+        insertIndex = 2 - 1;
+
+        while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+            arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+            insertIndex--;
+        }
+
+        arr[insertIndex + 1] = insertVal;
+        System.out.println("第2轮插入");
+        System.out.println(Arrays.toString(arr));
+
+
+        insertVal = arr[3];
+        insertIndex = 3 - 1;
+
+        while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+            arr[insertIndex + 1] = arr[insertIndex];// arr[insertIndex]
+            insertIndex--;
+        }
+
+        arr[insertIndex + 1] = insertVal;
+        System.out.println("第3轮插入");
+        System.out.println(Arrays.toString(arr));
     }
 
 }
