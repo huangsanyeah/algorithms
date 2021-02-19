0aac3a14bf6e8c9ab52207998550bb0263860285
hwy15972 <893663791@qq.com>
2021-02-19 15:04:53 +0800

    归并排序

--- src/datastructure/sort/MergetSort.java
+++ src/datastructure/sort/MergetSort.java
@@ -1,31 +1,37 @@
 package datastructure.sort;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Arrays;
 
+/**
+ * 归并排序
+ */
 public class MergetSort {
 
     public static void main(String[] args) {
-        //int arr[] = { 8, 4, 5, 7, 1, 3, 6, 2 }; //
+        int arr[] = {8, 4, 5, 7, 1, 3, 6, 2};
+        System.out.println("归并排序前=" + Arrays.toString(arr));
+        int temp[] = new int[arr.length]; //归并排序需要一个额外空间
+        mergeSort(arr, 0, arr.length - 1, temp);
+        System.out.println("归并排序后=" + Arrays.toString(arr));
 
         //测试快排的执行速度
         // 创建要给80000个的随机的数组
-        int[] arr = new int[8000000];
-        for (int i = 0; i < 8000000; i++) {
-            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
-        }
-        System.out.println("排序前");
-        Date data1 = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1Str = simpleDateFormat.format(data1);
-        System.out.println("排序前的时间是=" + date1Str);
-
-        int temp[] = new int[arr.length]; //归并排序需要一个额外空间
-        mergeSort(arr, 0, arr.length - 1, temp);
-
-        Date data2 = new Date();
-        String date2Str = simpleDateFormat.format(data2);
-        System.out.println("排序前的时间是=" + date2Str);
+//        int[] arr = new int[8000000];
+//        for (int i = 0; i < 8000000; i++) {
+//            arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+//        }
+//        System.out.println("排序前");
+//        Date data1 = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date1Str = simpleDateFormat.format(data1);
+//        System.out.println("排序前的时间是=" + date1Str);
+//
+//        int temp[] = new int[arr.length]; //归并排序需要一个额外空间
+//        mergeSort(arr, 0, arr.length - 1, temp);
+//
+//        Date data2 = new Date();
+//        String date2Str = simpleDateFormat.format(data2);
+//        System.out.println("排序前的时间是=" + date2Str);
 
         //System.out.println("归并排序后=" + Arrays.toString(arr));
     }
