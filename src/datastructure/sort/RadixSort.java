dc5c7e3ef7e0750357804424cb5487f71260955c
hwy15972 <893663791@qq.com>
2021-02-19 16:55:00 +0800

    归并排序注释

--- src/datastructure/sort/RadixSort.java
+++ src/datastructure/sort/RadixSort.java
@@ -1,30 +1,29 @@
 package datastructure.sort;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 
 public class RadixSort {
 
     public static void main(String[] args) {
-        int arr[] = {53, 3, 542, 748, 14, 214};
-
+        int[] arr = {53, 3, 542, 748, 14, 214};
+        System.out.println("基数排序前 " + Arrays.toString(arr));
+        radixSort(arr);
         // 80000000 * 11 * 4 / 1024 / 1024 / 1024 =3.3G
 //		int[] arr = new int[8000000];
 //		for (int i = 0; i < 8000000; i++) {
 //			arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
 //		}
-        System.out.println("排序前");
-        Date data1 = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String date1Str = simpleDateFormat.format(data1);
-        System.out.println("排序前的时间是=" + date1Str);
-
-        radixSort(arr);
-
-        Date data2 = new Date();
-        String date2Str = simpleDateFormat.format(data2);
-        System.out.println("排序前的时间是=" + date2Str);
+//        System.out.println("排序前");
+//        Date data1 = new Date();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String date1Str = simpleDateFormat.format(data1);
+//        System.out.println("排序前的时间是=" + date1Str);
+//
+//        radixSort(arr);
+//
+//        Date data2 = new Date();
+//        String date2Str = simpleDateFormat.format(data2);
+//        System.out.println("排序前的时间是=" + date2Str);
 
         System.out.println("基数排序后 " + Arrays.toString(arr));
 
