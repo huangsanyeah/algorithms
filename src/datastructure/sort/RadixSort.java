98188257479d477f0b471f335daa09a18b869b24
hwy15972 <893663791@qq.com>
2021-02-24 11:01:36 +0800

    基数排序练习

--- src/datastructure/sort/RadixSort.java
+++ src/datastructure/sort/RadixSort.java
@@ -8,13 +8,16 @@ import java.util.Arrays;
  * 复杂度：O（n*k）
  * n: 数据规模
  * k: “桶”的个数
+ * 参考百度百科：
+ * https://baike.baidu.com/item/%E5%9F%BA%E6%95%B0%E6%8E%92%E5%BA%8F/7875498?fr=aladdin
  */
 public class RadixSort {
 
     public static void main(String[] args) {
         int[] arr = {53, 3, 542, 748, 14, 214};
         System.out.println("基数排序前 " + Arrays.toString(arr));
-        radixSort(arr);
+        //radixSort(arr);
+        testRadixSort(arr);
         //FIXME 注意这里面的内存占用 因为需要耗费额外的空间  80000000 * 11 * 4 / 1024 / 1024 / 1024 =3.3G
 //		int[] arr = new int[8000000];
 //		for (int i = 0; i < 8000000; i++) {
@@ -178,5 +181,45 @@ public class RadixSort {
 		}
 		System.out.println("第3轮，对个位的排序处理 arr =" + Arrays.toString(arr)); */
 
+    }
+
+
+    public static void testRadixSort(int[] arr) {
+        //定义一个二维数组，表示10个桶, 每个桶就是一个一维数组
+        //说明
+        //1. 二维数组包含10个一维数组 每个一维数组就是一个桶  TODO 10是因为0-9刚好是10个
+        //2. 为了防止在放入数的时候，数据溢出，则每个一维数组(桶)，大小定为arr.length
+        //3. 名明确，基数排序是使用空间换时间的经典算法
+        int[][] bucket = new int[10][arr.length];
+
+        //为了记录每个桶中，实际存放了多少个数据,我们定义一个一维数组来记录各个桶的每次放入的数据个数
+        //可以这里理解
+        //比如：bucketElementCounts[0] , 记录的就是  bucket[0] 桶的放入数据个数
+        int[] bucketElementCounts = new int[10];
+        //第1轮(针对每个元素的个位进行排序处理)
+        for(int j = 0; j < arr.length; j++) {
+            //取出每个元素的个位的值
+            int digitOfElement = arr[j] / 1 % 10;
+            //放入到对应的桶中
+            bucket[digitOfElement][bucketElementCounts[digitOfElement]] = arr[j];
+            bucketElementCounts[digitOfElement]++;
+        }
+        //按照这个桶的顺序(一维数组的下标依次取出数据，放入原来数组)
+        int index = 0;
+        //遍历每一桶，并将桶中是数据，放入到原数组
+        for(int k = 0; k < bucketElementCounts.length; k++) {
+            //如果桶中，有数据，我们才放入到原数组
+            if(bucketElementCounts[k] != 0) {
+                //循环该桶即第k个桶(即第k个一维数组), 放入
+                for(int l = 0; l < bucketElementCounts[k]; l++) {
+                    //取出元素放入到arr
+                    arr[index++] = bucket[k][l];
+                }
+            }
+            //第l轮处理后，需要将每个 bucketElementCounts[k] = 0 ！！！！
+            bucketElementCounts[k] = 0;
+
+        }
+        System.out.println("第1轮，对个位的排序处理 arr =" + Arrays.toString(arr));
     }
 }
